1dfd02d96dc83aaa4a08ed39907f77f224ec3703
Mike McMurray <mike@mcmurray.co.nz>
2024-02-01 08:38:42 +0000

    Footer sg change. Updated Bing with image info

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'No rights reserved. All content is free to use.';
 
   return {
     name,
